1c1abd57fca855491e2427c7dd007af3a1387c96
Aditya <adityaprakash.bobby@gmail.com>
2019-03-27 23:13:15 +0530

    Add Init methods to store credentials

--- script.py
+++ script.py
@@ -1,46 +1,108 @@
+from __future__ import print_function
+
 import bs4
 import requests
 from getpass import getpass
+
+from configparser import ConfigParser
 
 TRIGGER_URL = 'http://192.168.1.1/'
 
 AUTH_URL = 'http://172.16.1.11:1000/fgtauth?'
 
-def init():
-    pass
+CONFIG_FILE = '.env'
 
-def login():
-    
-    ID = input("Enter your ID:")
-    
-    PASSWORD = getpass("Enter the password:")
-     
-    res = requests.get(TRIGGER_URL)
-    
-    soup = bs4.BeautifulSoup(res.content, 'html5lib')
+class ConfigClass():
 
-    login_data = {
-            'username' : ID,
-            'password' : PASSWORD,
-        }
-    
-    login_data['magic'] = soup.find('input', attrs={'name':'magic'})['value']
-    
-    _AUTH_URL = AUTH_URL + login_data['magic']
-    
-    # print(_AUTH_URL)
-    
-    with requests.Session() as ses:
-        
-        headers = {
-                'user-agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-             }
-        
-        res = ses.post(_AUTH_URL, data=login_data, headers=headers)
-        
-        print('Successfully logged in!')
+    def __init__(self, 
+                USERNAME=None, 
+                PASSWORD=None,
+                TRIGGER_URL='http://192.168.1.1/',
+                AUTH_URL='http://172.16.1.11:1000/fgtauth?'):
 
-        KEEPALIVE
+        self.USERNAME = USERNAME
+        self.PASSWORD = PASSWORD
+        self.TRIGGER_URL = TRIGGER_URL
+        self.AUTH_URL = AUTH_URL
+
+    def initConfig(self):
+
+        try:
+            configFile = open(CONFIG_FILE, 'r+')
+
+            config = ConfigParser()
+
+            config.read(CONFIG_FILE)
+            
+            try:
+                
+                self.USERNAME = config.get('USER_LOGIN', 'USERNAME')
+                self.PASSWORD = config.get('USER_LOGIN', 'PASSWORD')
+
+            except:
+
+                raise
+            
+        except Exception:
+
+            print('Login creadentials not found. Creating a new config file...')
+
+            self.USERNAME = input("Enter your username/id : ")
+            self.PASSWORD = getpass("Enter your password : ")
+
+            config = ConfigParser()
+
+            config['USER_LOGIN'] = {
+                        'USERNAME': self.USERNAME, 
+                        'PASSWORD': self.PASSWORD,
+                        }
+
+            with open(CONFIG_FILE, 'w') as configfile:
+                config.write(configfile)
+
+    def loginIntranet(self):
+        
+        res = requests.get(self.TRIGGER_URL)
+        
+        soup = bs4.BeautifulSoup(res.content, 'html5lib')
+
+        login_data = {
+                'username' : self.USERNAME,
+                'password' : self.PASSWORD,
+            }
+        
+        login_data['magic'] = soup.find('input', attrs={'name':'magic'})['value']
+        
+        _AUTH_URL = self.AUTH_URL + login_data['magic']
+                
+        with requests.Session() as ses:
+            
+            headers = {
+                    'user-agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+                }
+            
+            res = ses.post(_AUTH_URL, data=login_data, headers=headers)
+
+            # print(res.next)
+
+            soup = bs4.BeautifulSoup(res.content, 'html5lib')
+
+            FAILED = soup.find('h1', attrs={'class':'logo'})
+            
+            if FAILED != None:
+
+                FAILED = soup.find('h1', attrs={'class':'logo'}).text    
+
+            if res.status_code == 200 and FAILED == 'Authentication Failed':
+            
+                print('Try logging again with correct password and username.')
+            
+            else:
+
+                print('Successfully logged in!')
 
 if __name__ == '__main__':
-    login()
+    conf = ConfigClass()
+    conf.initConfig()
+    conf.loginIntranet()
+
