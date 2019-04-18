027ca3b7c153397a0e7c0d629e6e1f9d95a8ba89
Aditya <adityaprakash.bobby@gmail.com>
2019-04-18 09:04:20 +0530

    Added setup.py and MANIFEST.in (WIP)

---
+++ handle_signal.py
@@ -0,0 +1,15 @@
+import signal
+
+class handleSignals:
+
+    def __init__(self):
+        pass
+    
+    def __enter__():
+        # catch the signal of SIGINT or SIGTERM
+        pass
+
+    def __exit__():
+        # handle the signal for SIGINT and SIGTERM
+        # and exit the program
+        pass
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IIIT Login Authentication',
+    version='1.0',
+    description='A login automation from the CLI for servers w/ no GUI in IIIT',
+    author='Aditya Prakash',
+    author_email='adityaprakash.bobby@gmail.com',
+    packages=find_packages(),
+)
